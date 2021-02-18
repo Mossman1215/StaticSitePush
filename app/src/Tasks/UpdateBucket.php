f6246b69479c40664ded7b6bbf6410e9d34391ff
Moss Cantwell <moss@silverstripe.com>
2021-02-18 14:07:49 +1300

    use synchronous

--- app/src/Tasks/UpdateBucket.php
+++ app/src/Tasks/UpdateBucket.php
@@ -5,6 +5,8 @@ use SilverStripe\Core\Injector\Injector;
 use Psr\Log\LoggerInterface;
 use SilverStripe\Security\Security;
 use SilverStripe\Core\Environment;
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
 
 class UpdateBucket extends BuildTask
 {
@@ -15,8 +17,8 @@ class UpdateBucket extends BuildTask
     {
         $logger = Injector::inst()->get(LoggerInterface::class);
         #$promises = [];
-        $client = new \Aws\S3\S3Client([
-            'region'  => 'us-west-2',
+        $client = new S3Client([
+            'region'  => 'us-east-1',
             'version' => '2006-03-01',
         ]);
 
@@ -25,13 +27,6 @@ class UpdateBucket extends BuildTask
         $dest = sprintf("s3://%s/",$bucket);
 
         $manager = new \Aws\S3\Transfer($client, $source, $dest);
-        $promise = $manager->promise(); #start transfer
-        $promise->then(function ($value) {
-            echo 'Done! '.$value;
-        });
-        $promise->otherwise(function ($reason) use ($logger) {
-            $logger->error(sprintf('failed to upload file %s',$reason));
-        });
-        #$metaPromise = \GuzzleHttp\Promise\all($promises)->wait();
+        $manager->transfer();
     }
 }
